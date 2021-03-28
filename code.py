f29fe24266f240b5f5ed62e5feb06247e87dca99
Tahmid-Hossain <mstahmid0@gmail.com>
2021-03-28 13:37:11 +0600

    Updated

--- code.py
+++ code.py
@@ -1,32 +1,46 @@
 import pygame
 from pygame.locals import *
 
-def main():
+
+def initialise():
     # Initialise screen
     pygame.init()
-    screen_width = 1700
-    screen_height = 700
-    screen_size = (screen_width, screen_height)
+    global screen, screen_size
+    screen_size = (1700, 700)
     screen = pygame.display.set_mode(screen_size)
     pygame.display.set_caption('Basic Pygame program')
 
+def _background():
     # Fill background
+    global background, background_rect
     background = pygame.Surface(screen.get_size())
     print(background)
     background = pygame.image.load("background.png")
     background = background.convert()
     background = pygame.transform.scale(background, screen_size)
     print(pygame.Surface.get_size(background))
-    background.get_rect()
+    background_rect = background.get_rect()
     # background.fill((250, 250, 250))
 
+def text():
     # Display some text
+    global text, font, textpos 
     font = pygame.font.Font(None, 36)
     text = font.render("Hello There", 1, (10, 10, 10))
     textpos = text.get_rect()
     textpos.centerx = background.get_rect().centerx
     background.blit(text, textpos)
 
+def bird():
+    # Importing the image of the bird.
+    global bird, bird_rect
+    bird = pygame.image.load("dove.png")
+    bird = pygame.transform.flip(bird, True, False)
+    bird_rect = bird.get_rect()
+    bird = pygame.transform.scale(bird, (60, 60))
+
+
+def blitting():
     # Blit everything to the screen
     screen.blit(background, screen_size)
     pygame.display.flip()
@@ -37,8 +51,16 @@ def main():
             if event.type == QUIT:
                 return
 
-        screen.blit(background, (0, 0))
+        # Update image.
+        screen.blit(background, background_rect)
+        screen.blit(bird, bird_rect)
         pygame.display.flip()
 
+def main():
+    initialise()
+    _background()
+    text()
+    bird()
+    blitting()
 
 if __name__ == '__main__': main()
